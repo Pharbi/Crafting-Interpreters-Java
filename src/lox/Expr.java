525d325a52011183cf3fe94cf7b158c15dcf6b47
gK <yankskwesi@gmail.com>
2022-12-01 01:12:15 -0500

    Fucking with formatting

--- src/lox/Expr.java
+++ src/lox/Expr.java
@@ -3,103 +3,127 @@ package lox;
 import java.util.List;
 
 abstract class Expr {
- interface Visitor<R> {
- R visitBinaryExpr(Binary expr);
- R visitGroupingExpr(Grouping expr);
- R visitLiteralExpr(Literal expr);
- R visitUnaryExpr(Unary expr);
- R visitVariableExpr(Variable expr);
- }
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
 
- abstract <R> R accept(Visitor<R> visitor);
- interface VisitorRPN<R> {
- R visitBinaryExprRPN(Binary expr);
- R visitGroupingExprRPN(Grouping expr);
- R visitLiteralExprRPN(Literal expr);
- R visitUnaryExprRPN(Unary expr);
- R visitVariableExprRPN(Variable expr);
- }
+    R visitGroupingExpr(Grouping expr);
 
- abstract <R> R acceptRPN(VisitorRPN<R> visitor);
- static class Binary extends Expr {
- final Expr left;
- final Token operator;
- final Expr right;
- Binary(Expr left, Token operator, Expr right) {
- this.left = left;
- this.operator = operator;
- this.right = right;
- }
+    R visitLiteralExpr(Literal expr);
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitBinaryExpr(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitBinaryExprRPN(this);
- }
- }
- static class Grouping extends Expr {
- final Expr expression;
- Grouping(Expr expression) {
- this.expression = expression;
- }
+    R visitUnaryExpr(Unary expr);
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitGroupingExpr(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitGroupingExprRPN(this);
- }
- }
- static class Literal extends Expr {
- final Object value;
- Literal(Object value) {
- this.value = value;
- }
+    R visitVariableExpr(Variable expr);
+  }
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitLiteralExpr(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitLiteralExprRPN(this);
- }
- }
- static class Unary extends Expr {
- final Token operator;
- final Expr right;
- Unary(Token operator, Expr right) {
- this.operator = operator;
- this.right = right;
- }
+  abstract <R> R accept(Visitor<R> visitor);
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitUnaryExpr(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitUnaryExprRPN(this);
- }
- }
- static class Variable extends Expr {
- final Token name;
- Variable(Token name) {
- this.name = name;
- }
+  interface VisitorRPN<R> {
+    R visitBinaryExprRPN(Binary expr);
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitVariableExpr(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitVariableExprRPN(this);
- }
- }
+    R visitGroupingExprRPN(Grouping expr);
+
+    R visitLiteralExprRPN(Literal expr);
+
+    R visitUnaryExprRPN(Unary expr);
+
+    R visitVariableExprRPN(Variable expr);
+  }
+
+  abstract <R> R acceptRPN(VisitorRPN<R> visitor);
+
+  static class Binary extends Expr {
+    final Expr left;
+    final Token operator;
+    final Expr right;
+
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitBinaryExprRPN(this);
+    }
+  }
+
+  static class Grouping extends Expr {
+    final Expr expression;
+
+    Grouping(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitGroupingExprRPN(this);
+    }
+  }
+
+  static class Literal extends Expr {
+    final Object value;
+
+    Literal(Object value) {
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitLiteralExprRPN(this);
+    }
+  }
+
+  static class Unary extends Expr {
+    final Token operator;
+    final Expr right;
+
+    Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitUnaryExprRPN(this);
+    }
+  }
+
+  static class Variable extends Expr {
+    final Token name;
+
+    Variable(Token name) {
+      this.name = name;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitVariableExprRPN(this);
+    }
+  }
 }
--- src/lox/Stmt.java
+++ src/lox/Stmt.java
@@ -3,65 +3,79 @@ package lox;
 import java.util.List;
 
 abstract class Stmt {
- interface Visitor<R> {
- R visitExpressionStmt(Expression stmt);
- R visitPrintStmt(Print stmt);
- R visitVarStmt(Var stmt);
- }
+  interface Visitor<R> {
+    R visitExpressionStmt(Expression stmt);
 
- abstract <R> R accept(Visitor<R> visitor);
- interface VisitorRPN<R> {
- R visitExpressionStmtRPN(Expression stmt);
- R visitPrintStmtRPN(Print stmt);
- R visitVarStmtRPN(Var stmt);
- }
+    R visitPrintStmt(Print stmt);
 
- abstract <R> R acceptRPN(VisitorRPN<R> visitor);
- static class Expression extends Stmt {
- final Expr expression;
- Expression(Expr expression) {
- this.expression = expression;
- }
+    R visitVarStmt(Var stmt);
+  }
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitExpressionStmt(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitExpressionStmtRPN(this);
- }
- }
- static class Print extends Stmt {
- final Expr expression;
- Print(Expr expression) {
- this.expression = expression;
- }
+  abstract <R> R accept(Visitor<R> visitor);
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitPrintStmt(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitPrintStmtRPN(this);
- }
- }
- static class Var extends Stmt {
- final Token name;
- final Expr initializer;
- Var(Token name, Expr initializer) {
- this.name = name;
- this.initializer = initializer;
- }
+  interface VisitorRPN<R> {
+    R visitExpressionStmtRPN(Expression stmt);
 
- @Override
- <R> R accept(Visitor<R> visitor) {
- return visitor.visitVarStmt(this);
- }
- @Override
- <R> R acceptRPN(VisitorRPN<R> visitor) {
- return visitor.visitVarStmtRPN(this);
- }
- }
+    R visitPrintStmtRPN(Print stmt);
+
+    R visitVarStmtRPN(Var stmt);
+  }
+
+  abstract <R> R acceptRPN(VisitorRPN<R> visitor);
+
+  static class Expression extends Stmt {
+    final Expr expression;
+
+    Expression(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitExpressionStmt(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitExpressionStmtRPN(this);
+    }
+  }
+
+  static class Print extends Stmt {
+    final Expr expression;
+
+    Print(Expr expression) {
+      this.expression = expression;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitPrintStmt(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitPrintStmtRPN(this);
+    }
+  }
+
+  static class Var extends Stmt {
+    final Token name;
+    final Expr initializer;
+
+    Var(Token name, Expr initializer) {
+      this.name = name;
+      this.initializer = initializer;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVarStmt(this);
+    }
+
+    @Override
+    <R> R acceptRPN(VisitorRPN<R> visitor) {
+      return visitor.visitVarStmtRPN(this);
+    }
+  }
 }
